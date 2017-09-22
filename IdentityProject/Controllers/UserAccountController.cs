40079175ca14e1e3c46a175e03926a4ff136dd91
Igor Belousov <igbe@ciklum.com>
2017-09-22 12:41:38 +0300

    Added a new userAccount controller + login page.

---
+++ IdentityProject/Controllers/UserAccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IdentityProject.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
