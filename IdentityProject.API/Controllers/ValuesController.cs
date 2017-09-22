0efe0b1ca2f17676382398bd8e9cd885b24c2c4e
Igor Belousov <igbe@ciklum.com>
2017-09-22 14:22:13 +0300

    Installed IdentityModel

--- IdentityProject.API/Controllers/ValuesController.cs
+++ IdentityProject.API/Controllers/ValuesController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityProject.API.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     public class ValuesController : Controller
     {
         // GET api/values
